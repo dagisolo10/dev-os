87d42dc211a102bfa1cd6231ca1333aa33044a32
Dagmawi Solomon Tilahun <dagisolo1023@gmail.com>
2026-03-29 19:45:58 +0300

    project query fixed in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,13 @@
 import prisma from "@/lib/prisma";
 import { MetadataRoute } from "next";
+import { slugify } from "@/lib/helper-functions";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const baseUrl = "https://dev-os-org.vercel.app";
 
-    const projects = await prisma.project.findMany({ select: { slug: true, id: true, updatedAt: true } });
+    const projects = await prisma.project.findMany({ select: { title: true, id: true, updatedAt: true } });
 
-    const projectEntries = projects.map((p) => ({ url: `${baseUrl}/${p.slug}-${p.id}`, lastModified: p.updatedAt, changeFrequency: "weekly" as const, priority: 0.8 }));
+    const projectEntries = projects.map((p) => ({ url: `${baseUrl}/${slugify(p.title)}-${p.id}`, lastModified: p.updatedAt, changeFrequency: "weekly" as const, priority: 0.8 }));
 
     return [{ url: baseUrl, lastModified: new Date(), changeFrequency: "daily", priority: 1 }, ...projectEntries];
 }
